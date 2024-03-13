0125b47479f7ba2ac5583acf106c6cb8001f93d8
Mateusz Gańko <mateuszganko@netsprint.eu>
2024-03-13 15:41:26 +0100

    wattTime client

---
+++ src/main/java/com/ing/hackathon/watttime/WattTimeClient.java
@@ -0,0 +1,18 @@
+package com.ing.hackathon.watttime;
+
+public class WattTimeClient {
+
+    private static final String URL = "https://api.watttime.org/register";
+
+    public WattTimeClient() {
+
+    }
+
+    public void getToken() {
+
+    }
+
+    public void register(String username, String password, String email) {
+
+    }
+}
